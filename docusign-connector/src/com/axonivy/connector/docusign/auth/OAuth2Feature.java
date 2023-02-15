f5dfdcfd948dbf1d7834759b29bef8be5e2ed67e
Peter Stieglecker <peter.stieglecker@axonivy.com>
2023-02-15 13:01:41 +0100

    adapted to original code style

--- docusign-connector/src/com/axonivy/connector/docusign/auth/OAuth2Feature.java
+++ docusign-connector/src/com/axonivy/connector/docusign/auth/OAuth2Feature.java
@@ -26,141 +26,142 @@ import ch.ivyteam.ivy.security.ISession;
  * @since 9.2
  */
 public class OAuth2Feature implements Feature {
-	public static interface Property {
-		String CLIENT_ID = "AUTH.integrationKey";
-		String USER_KEY = "AUTH.secretKey";
-		String SCOPE = "AUTH.scope";
-		String ACCOUNT_ID = "AUTH.accountId";
-		String JWT_USER_ID = "AUTH.jwtUserId";
-		String JWT_KEY_FILE = "AUTH.jwtKeyFile";
-		String JWT_USE = "AUTH.jwtUse";
-		String AUTH_BASE_URI = "AUTH.baseUri";
-	}
 
-	public static final String SESSION_TOKEN = "docu.sign.authCode";
+  public static interface Property {
 
-	@Override
-	public boolean configure(FeatureContext context) {
-		var config = new FeatureConfig(context.getConfiguration(), OAuth2Feature.class);
-		var docuSignUri = new OAuth2UriProperty(config, Property.AUTH_BASE_URI, "https://account-d.docusign.com/oauth");
-		var oauth2 = new OAuth2BearerFilter(
-				ctxt -> requestToken(ctxt, docuSignUri),
-				docuSignUri);
-		context.register(oauth2, Priorities.AUTHORIZATION);
+    String CLIENT_ID = "AUTH.integrationKey";
+    String USER_KEY = "AUTH.secretKey";
+    String SCOPE = "AUTH.scope";
+    String ACCOUNT_ID = "AUTH.accountId";
+    String JWT_USER_ID = "AUTH.jwtUserId";
+    String JWT_KEY_FILE = "AUTH.jwtKeyFile";
+    String JWT_USE = "AUTH.jwtUse";
+    String AUTH_BASE_URI = "AUTH.baseUri";
+  }
 
-		context.register(new UserUriFilter(ISession.current(), docuSignUri), Priorities.AUTHORIZATION + 10);
-		return true;
-	}
+  public static final String SESSION_TOKEN = "docu.sign.authCode";
 
-	/**
-	 * Get token.
-	 * 
-	 * Default is to get a web user grant token. If the current user is system or if
-	 * configuration is set to JWT, use a JWT grant.
-	 * 
-	 * @param ctxt
-	 * @param uriFactory
-	 * @return
-	 */
-	private static Response requestToken(AuthContext ctxt, OAuth2UriProperty uriFactory) {
-		ISession current = ISession.current();
-		if(current.getIdentifier() == ISecurityConstants.SYSTEM_USER_SESSION_ID ||
-				isTrue(ctxt.config.read(Property.JWT_USE))) {
-			return jwtGrantToken(ctxt, uriFactory);
-		}
-		return webUserGrantToken(ctxt, uriFactory);
-	}
+  @Override
+  public boolean configure(FeatureContext context) {
+    var config = new FeatureConfig(context.getConfiguration(), OAuth2Feature.class);
+    var docuSignUri = new OAuth2UriProperty(config, Property.AUTH_BASE_URI,
+            "https://account-d.docusign.com/oauth");
+    var oauth2 = new OAuth2BearerFilter(
+            ctxt -> requestToken(ctxt, docuSignUri),
+            docuSignUri);
+    context.register(oauth2, Priorities.AUTHORIZATION);
+    context.register(new UserUriFilter(ISession.current(), docuSignUri), Priorities.AUTHORIZATION + 10);
+    return true;
+  }
 
-	/**
-	 * Use the JWT grant?
-	 * 
-	 * @param use
-	 * @return
-	 */
-	public static boolean isTrue(Optional<String> use) {
-		return use.filter(val -> !val.isBlank() && Boolean.parseBoolean(val)).isPresent();
-	}
+  /**
+   * Get token.
+   * 
+   * Default is to get a web user grant token. If the current user is system or
+   * if configuration is set to JWT, use a JWT grant.
+   * 
+   * @param ctxt
+   * @param uriFactory
+   * @return
+   */
+  private static Response requestToken(AuthContext ctxt, OAuth2UriProperty uriFactory) {
+    ISession current = ISession.current();
+    if (current.getIdentifier() == ISecurityConstants.SYSTEM_USER_SESSION_ID ||
+            isTrue(ctxt.config.read(Property.JWT_USE))) {
+      return jwtGrantToken(ctxt, uriFactory);
+    }
+    return webUserGrantToken(ctxt, uriFactory);
+  }
 
-	private static Response webUserGrantToken(AuthContext ctxt, OAuth2UriProperty uriFactory) {
-		var authCode = ctxt.authCode();
-		var refreshToken = ctxt.refreshToken();
-		if (authCode.isEmpty() && refreshToken.isEmpty()) {
-			authRedirectError(ctxt.config, uriFactory).throwError();
-		}
+  /**
+   * Use the JWT grant?
+   * 
+   * @param use
+   * @return
+   */
+  public static boolean isTrue(Optional<String> use) {
+    return use.filter(val -> !val.isBlank() && Boolean.parseBoolean(val)).isPresent();
+  }
 
-		var clientId = ctxt.config.readMandatory(Property.CLIENT_ID);
-		var userKey = ctxt.config.readMandatory(Property.USER_KEY);
-		var basicAuth = HttpBasicAuthenticationFeature.basic(clientId, userKey);
-		Object authRequest;
-		if (authCode.isPresent()) {
-			authRequest = new DocuSignAuthRequest(authCode.get());
-		} else {
-			authRequest = new DocuSignRefreshTokenRequest(refreshToken.get());
-		}
+  private static Response webUserGrantToken(AuthContext ctxt, OAuth2UriProperty uriFactory) {
+    var authCode = ctxt.authCode();
+    var refreshToken = ctxt.refreshToken();
+    if (authCode.isEmpty() && refreshToken.isEmpty()) {
+      authRedirectError(ctxt.config, uriFactory).throwError();
+    }
+    var clientId = ctxt.config.readMandatory(Property.CLIENT_ID);
+    var userKey = ctxt.config.readMandatory(Property.USER_KEY);
+    var basicAuth = HttpBasicAuthenticationFeature.basic(clientId, userKey);
+    Object authRequest;
+    if (authCode.isPresent()) {
+      authRequest = new DocuSignAuthRequest(authCode.get());
+    } else {
+      authRequest = new DocuSignRefreshTokenRequest(refreshToken.get());
+    }
+    var response = ctxt.target
+            .register(basicAuth)
+            .request()
+            .post(Entity.json(authRequest));
+    return response;
+  }
 
-		var response = ctxt.target
-				.register(basicAuth)
-				.request()
-				.post(Entity.json(authRequest));
-		return response;
-	}
+  public static class DocuSignAuthRequest {
 
-	public static class DocuSignAuthRequest {
-		public String grant_type;
-		public String code;
+    public String grant_type;
+    public String code;
 
-		public DocuSignAuthRequest(String code) {
-			this.grant_type = "authorization_code";
-			this.code = code;
-		}
-	}
+    public DocuSignAuthRequest(String code) {
+      this.grant_type = "authorization_code";
+      this.code = code;
+    }
+  }
 
-	public static class DocuSignRefreshTokenRequest {
-		public String grant_type;
-		public String refresh_token;
+  public static class DocuSignRefreshTokenRequest {
 
-		public DocuSignRefreshTokenRequest(String refreshToken) {
-			this.grant_type = "refresh_token";
-			this.refresh_token = refreshToken;
-		}
-	}
+    public String grant_type;
+    public String refresh_token;
 
-	private static BpmPublicErrorBuilder authRedirectError(FeatureConfig config, OAuth2UriProperty uriFactory) {
-		URI redirectUri = OAuth2CallbackUriBuilder.create().toUrl();
-		var uri = UriBuilder.fromUri(uriFactory.getUri("auth"))
-				.queryParam("response_type", "code")
-				.queryParam("scope", getScope(config))
-				.queryParam("client_id", config.readMandatory(Property.CLIENT_ID))
-				.queryParam("redirect_uri", redirectUri)
-				.build();
-		Ivy.log().debug("created oauth URI: " + uri);
+    public DocuSignRefreshTokenRequest(String refreshToken) {
+      this.grant_type = "refresh_token";
+      this.refresh_token = refreshToken;
+    }
+  }
 
-		return OAuth2RedirectErrorBuilder
-				.create(uri)
-				.withMessage("Missing permission from user to act in his name.");
-	}
+  private static BpmPublicErrorBuilder authRedirectError(FeatureConfig config, OAuth2UriProperty uriFactory) {
+    URI redirectUri = OAuth2CallbackUriBuilder.create().toUrl();
+    var uri = UriBuilder.fromUri(uriFactory.getUri("auth"))
+            .queryParam("response_type", "code")
+            .queryParam("scope", getScope(config))
+            .queryParam("client_id", config.readMandatory(Property.CLIENT_ID))
+            .queryParam("redirect_uri", redirectUri)
+            .build();
+    Ivy.log().debug("created oauth URI: " + uri);
+    return OAuth2RedirectErrorBuilder
+            .create(uri)
+            .withMessage("Missing permission from user to act in his name.");
+  }
 
-	private static Response jwtGrantToken(AuthContext ctxt, OAuth2UriProperty uriFactory) {
-		String token = new JwtFactory(ctxt.config, uriFactory).createToken();
-		var authRequest = new DocuSignJwtRequest(token);
-		var response = ctxt.target
-				.request()
-				.post(Entity.json(authRequest));
-		return response;
-	}
+  private static Response jwtGrantToken(AuthContext ctxt, OAuth2UriProperty uriFactory) {
+    String token = new JwtFactory(ctxt.config, uriFactory).createToken();
+    var authRequest = new DocuSignJwtRequest(token);
+    var response = ctxt.target
+            .request()
+            .post(Entity.json(authRequest));
+    return response;
+  }
 
-	public static class DocuSignJwtRequest {
-		public String grant_type;
-		public String assertion;
+  public static class DocuSignJwtRequest {
 
-		public DocuSignJwtRequest(String jwtToken) {
-			this.grant_type = "urn:ietf:params:oauth:grant-type:jwt-bearer";
-			this.assertion = jwtToken;
-		}
-	}
+    public String grant_type;
+    public String assertion;
 
-	static String getScope(FeatureConfig config) {
-		return config.read(Property.SCOPE).orElse("signature impersonation");
-	}
+    public DocuSignJwtRequest(String jwtToken) {
+      this.grant_type = "urn:ietf:params:oauth:grant-type:jwt-bearer";
+      this.assertion = jwtToken;
+    }
+  }
 
+  static String getScope(FeatureConfig config) {
+    return config.read(Property.SCOPE).orElse("signature impersonation");
+  }
 }
--- docusign-connector/src/com/axonivy/connector/docusign/auth/UserUriFilter.java
+++ docusign-connector/src/com/axonivy/connector/docusign/auth/UserUriFilter.java
@@ -28,128 +28,118 @@ import ch.ivyteam.ivy.security.ISession;
  */
 @Provider
 public class UserUriFilter implements javax.ws.rs.client.ClientRequestFilter {
-	/**
-	 * Placeholder to replace with account id. Note: the value here
-	 * must match the word configured in the Rest client property
-	 * PATH.accountId.
-	 */
-	public static final String ACCOUNT_ID_PLACEHOLDER = "placeholder"; 
 
-	private static final String USER_INFO = "docusign.userInfo";
+  /**
+   * Placeholder to replace with account id. Note: the value here must match the
+   * word configured in the Rest client property PATH.accountId.
+   */
+  public static final String ACCOUNT_ID_PLACEHOLDER = "placeholder";
+  private static final String USER_INFO = "docusign.userInfo";
+  private final ISession session;
+  private final OAuth2UriProperty uriFactory;
 
-	private final ISession session;
-	private final OAuth2UriProperty uriFactory;
+  public UserUriFilter(ISession session, OAuth2UriProperty uriFactory) {
+    this.session = session;
+    this.uriFactory = uriFactory;
+  }
 
-	public UserUriFilter(ISession session, OAuth2UriProperty uriFactory) {
-		this.session = session;
-		this.uriFactory = uriFactory;
-	}
+  @Override
+  public void filter(ClientRequestContext context) throws IOException {
+    if (uriFactory.isAuthRequest(context.getUri())) { // do not intercept token
+      // request: avoid
+      // stackOverFlow!
+      return;
+    }
+    JsonNode userInfo = readUserInfo(session);
+    if (userInfo == null) {
+      userInfo = context.getClient()
+              .target(uriFactory.getUri("userinfo"))
+              .request()
+              .headers(context.getHeaders()) // copy bearer token
+              .get().readEntity(JsonNode.class);
+      session.setAttribute(USER_INFO, userInfo);
+    }
+    String accountId = (String) context.getConfiguration().getProperty(OAuth2Feature.Property.ACCOUNT_ID);
+    JsonNode account = getAccount(userInfo, accountId);
+    URI userUri = routeToUserUri(context.getUri(), account);
+    context.setUri(userUri);
+  }
 
-	@Override
-	public void filter(ClientRequestContext context) throws IOException {
-		if (uriFactory.isAuthRequest(context.getUri())) { // do not intercept token
-			// request: avoid
-			// stackOverFlow!
-			return;
-		}
+  public static JsonNode readUserInfo(ISession s) {
+    return (JsonNode) s.getAttribute(USER_INFO);
+  }
 
-		JsonNode userInfo = readUserInfo(session);
-		if (userInfo == null) {
-			userInfo = context.getClient()
-					.target(uriFactory.getUri("userinfo"))
-					.request()
-					.headers(context.getHeaders()) // copy bearer token
-					.get().readEntity(JsonNode.class);
-			session.setAttribute(USER_INFO, userInfo);
-		}
+  /**
+   * Extract the account to use.
+   * 
+   * A user might have multiple accounts. If the accountId is set, then the
+   * corresponding account will be searched. If it is not set or if it is not
+   * found, then take the default.
+   * 
+   * @param accounts
+   * @param accountId
+   * @return
+   */
+  private static JsonNode getAccount(JsonNode accounts, String accountId) {
+    JsonNode account = null;
+    boolean hasAccountId = StringUtils.isNotBlank(accountId);
+    if (accounts != null) {
+      JsonNode accountsList = accounts.get("accounts");
+      if (accountsList != null) {
+        account = accountsList.get(0);
+        for (JsonNode acc : accountsList) {
+          if (hasAccountId && accountId.equalsIgnoreCase(getAccountId(acc))) {
+            account = acc;
+            break;
+          } else if (isDefault(acc)) {
+            account = acc;
+            break;
+          }
+        }
+      }
+    }
+    if (account == null) {
+      Ivy.log().warn("Could not find any DocuSign account while searching for {0}.",
+              hasAccountId ? accountId : "any");
+    } else if (hasAccountId && !accountId.equalsIgnoreCase(getAccountId(account))) {
+      Ivy.log().warn("Could not find DocuSign account with id ''{0}'', using {1}.", accountId,
+              accountToString(account));
+    } else if (!hasAccountId && !isDefault(account)) {
+      Ivy.log().warn("Could not find default DocuSign account, using {1}.", accountToString(account));
+    }
+    return account;
+  }
 
-		String accountId = (String)context.getConfiguration().getProperty(OAuth2Feature.Property.ACCOUNT_ID);
-		JsonNode account = getAccount(userInfo, accountId);
-		URI userUri = routeToUserUri(context.getUri(), account);
-		context.setUri(userUri);
-	}
+  private static URI routeToUserUri(URI uri, JsonNode accountInfo) {
+    Ivy.log().debug("patching URI: " + uri);
+    String resource = StringUtils.substringAfter(uri.getPath(), ACCOUNT_ID_PLACEHOLDER + "/");
+    String rawQuery = uri.getRawQuery();
+    URI baseUri = URI.create(accountInfo.get("base_uri").asText());
+    URI userUri = UriBuilder.fromUri(baseUri).path("/restapi/v2.1/accounts/{myId}")
+            .resolveTemplate("myId", getAccountId(accountInfo))
+            .path(resource)
+            .replaceQuery(rawQuery)
+            .build();
+    Ivy.log().debug("patched user URI: " + userUri);
+    return userUri;
+  }
 
-	public static JsonNode readUserInfo(ISession s) {
-		return (JsonNode) s.getAttribute(USER_INFO);
-	}
+  private static boolean isDefault(JsonNode account) {
+    return account.get("is_default").asBoolean();
+  }
 
-	/**
-	 * Extract the account to use.
-	 * 
-	 * A user might have multiple accounts. If the accountId is set, then
-	 * the corresponding account will be searched. If it is not set or
-	 * if it is not found, then take the default.
-	 * 
-	 * @param accounts
-	 * @param accountId
-	 * @return
-	 */
-	private static JsonNode getAccount(JsonNode accounts, String accountId) {
-		JsonNode account = null;
-		boolean hasAccountId = StringUtils.isNotBlank(accountId);
+  private static String getAccountName(JsonNode account) {
+    return account.get("account_name").asText();
+  }
 
-		if(accounts != null) {
-			JsonNode accountsList = accounts.get("accounts");
-			if(accountsList != null) {
-				account = accountsList.get(0);
-				for (JsonNode acc : accountsList) {
-					if(hasAccountId && accountId.equalsIgnoreCase(getAccountId(acc))) {
-						account = acc;
-						break;
-					}
-					else if(isDefault(acc)) {
-						account = acc;
-						break;
-					}
-				}
-			}
-		}
+  private static String getAccountId(JsonNode account) {
+    return account.get("account_id").asText();
+  }
 
-		if(account == null) {
-			Ivy.log().warn("Could not find any DocuSign account while searching for {0}.", hasAccountId ? accountId : "any");
-		}
-		else if(hasAccountId && !accountId.equalsIgnoreCase(getAccountId(account))) {
-			Ivy.log().warn("Could not find DocuSign account with id ''{0}'', using {1}.", accountId, accountToString(account));
-		}
-		else if(!hasAccountId && !isDefault(account)) {
-			Ivy.log().warn("Could not find default DocuSign account, using {1}.", accountToString(account));
-		}
-
-		return account;
-	}
-
-	private static URI routeToUserUri(URI uri, JsonNode accountInfo) {
-		Ivy.log().debug("patching URI: " + uri);
-		String resource = StringUtils.substringAfter(uri.getPath(), ACCOUNT_ID_PLACEHOLDER + "/");
-		String rawQuery = uri.getRawQuery();
-
-		URI baseUri = URI.create(accountInfo.get("base_uri").asText());
-		URI userUri = UriBuilder.fromUri(baseUri).path("/restapi/v2.1/accounts/{myId}")
-				.resolveTemplate("myId", getAccountId(accountInfo))
-				.path(resource)
-				.replaceQuery(rawQuery)
-				.build();
-		Ivy.log().debug("patched user URI: " + userUri);
-
-		return userUri;
-	}
-
-	private static boolean isDefault(JsonNode account) {
-		return account.get("is_default").asBoolean();
-	}
-
-	private static String getAccountName(JsonNode account) {
-		return account.get("account_name").asText();
-	}
-
-	private static String getAccountId(JsonNode account) {
-		return account.get("account_id").asText();
-	}
-
-	private static String accountToString(JsonNode account) {
-		return MessageFormat.format("Account: Id: ''{0}'' Name: ''{1}'' Default: {2}}",
-				getAccountId(account),
-				getAccountName(account),
-				isDefault(account));
-	}
+  private static String accountToString(JsonNode account) {
+    return MessageFormat.format("Account: Id: ''{0}'' Name: ''{1}'' Default: {2}}",
+            getAccountId(account),
+            getAccountName(account),
+            isDefault(account));
+  }
 }
