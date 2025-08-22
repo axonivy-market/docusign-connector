8416603de0074bbfcc304d8c8f99aa3b215f737a
Trần Vĩnh Thiện Phúc <143604440+tvtphuc-axonivy@users.noreply.github.com>
2025-08-22 16:29:40 +0700

    MARP-2631 Removed unused codes (#87)

--- docusign-connector-test/src_test/com/axonivy/connector/docusign/test/TestEnvelopeAPI.java
+++ docusign-connector-test/src_test/com/axonivy/connector/docusign/test/TestEnvelopeAPI.java
@@ -2,6 +2,7 @@ package com.axonivy.connector.docusign.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static com.axonivy.utils.e2etest.enums.E2EEnvironment.REAL_SERVER;
 
 import java.io.IOException;
 import org.apache.commons.lang3.ObjectUtils;
@@ -12,8 +13,8 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.Assumptions;
 
 import com.axonivy.connector.docusign.test.constants.DocusignTestConstants;
-import com.axonivy.connector.docusign.test.context.MultiEnvironmentContextProvider;
-import com.axonivy.connector.docusign.test.utils.DocusignTestUtils;
+import com.axonivy.utils.e2etest.context.MultiEnvironmentContextProvider;
+import com.axonivy.utils.e2etest.utils.E2ETestUtils;
 import com.docusign.esign.model.EnvelopeDefinition;
 
 import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
@@ -29,7 +30,7 @@ public class TestEnvelopeAPI {
 
   @BeforeEach
   void beforeEach(AppFixture fixture, ExtensionContext context) throws Exception {
-    DocusignTestUtils.setUpConfigForContext(context.getDisplayName(), fixture);
+    E2ETestUtils.determineConfigForContext(context.getDisplayName(), runRealEnv(fixture), runMockEnv());
   }
 
   private interface Start {
@@ -37,13 +38,32 @@ public class TestEnvelopeAPI {
   }
 
   @TestTemplate
-  void callSubProcess_createEnvelope(BpmClient bpmClient, ExtensionContext context) throws IOException, NoSuchFieldException {
-    Assumptions.assumeTrue(context.getDisplayName().equals(DocusignTestConstants.REAL_CALL_CONTEXT_DISPLAY_NAME), "Skipping test in PROD environment");
+  void callSubProcess_createEnvelope(BpmClient bpmClient, ExtensionContext context)
+      throws IOException, NoSuchFieldException {
+    Assumptions.assumeTrue(context.getDisplayName().equals(REAL_SERVER.getDisplayName()),
+        "Skipping test in PROD environment");
     var result = bpmClient.start().subProcess(Start.CREATE_ENVELOPE).execute(new EnvelopeDefinition());
     String redirectUrl = result.http().redirectLocation();
     assertTrue(ObjectUtils.isNotEmpty(redirectUrl));
     assertThat(redirectUrl).containsSubsequence(
         "https://account-d.docusign.com/oauth/auth?response_type=code&scope=signature+impersonation&client_id=",
         "&redirect_uri=", "oauth2%2Fcallback&state=");
+  }
+
+  private Runnable runRealEnv(AppFixture fixture) {
+    return () -> {
+      String intergrationKey = System.getProperty(DocusignTestConstants.INTERGRATION_KEY);
+      String secretKey = System.getProperty(DocusignTestConstants.SECRET_KEY);
+      String accountId = System.getProperty(DocusignTestConstants.ACCOUNT_ID);
+      fixture.var("docusignConnector.integrationKey", intergrationKey);
+      fixture.var("docusignConnector.secretKey", secretKey);
+      fixture.var("docusignConnector.accountId", accountId);
+      fixture.var("returnPage", "http://localhost:8080/");
+      fixture.var("frameAncestors", "http://localhost:8080/, https://apps-d.docusign.com");
+    };
+  }
+
+  private Runnable runMockEnv() {
+    return () -> {};
   }
 }
--- docusign-connector-test/src_test/com/axonivy/connector/docusign/test/constants/DocusignTestConstants.java
+++ docusign-connector-test/src_test/com/axonivy/connector/docusign/test/constants/DocusignTestConstants.java
@@ -1,11 +1,7 @@
 package com.axonivy.connector.docusign.test.constants;
 
 public class DocusignTestConstants {
-  public static final String REAL_CALL_CONTEXT_DISPLAY_NAME = "Real Server Test";
-  public static final String MOCK_SERVER_CONTEXT_DISPLAY_NAME = "Mock Server Test";
   public static final String INTERGRATION_KEY = "integrationKey";
   public static final String SECRET_KEY = "secretKey";
   public static final String ACCOUNT_ID = "accountId";
-  public static final String END_TO_END_TESTING_ENVIRONMENT_KEY = "testEnvironment";
-  public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E";
 }
